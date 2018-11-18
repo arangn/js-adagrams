e247ce5d0ed3cb3af0eaf18d4d67a585e0d759b2
Naheed Arang <arang.na@gmail.com>
2018-11-17 18:50:54 -0800

    wave one pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,44 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    const letters = [
+      "A", "A", "A", "A", "A", "A", "A", "A", "A",
+      "B", "B",
+      "C", "C",
+      "D", "D", "D", "D",
+      "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+      "F", "F",
+      "G", "G", "G",
+      "H", "H",
+      "I", "I", "I", "I", "I", "I", "I", "I", "I",
+      "J",
+      "K",
+      "L", "L", "L", "L",
+      "M", "M",
+      "N", "N", "N", "N", "N", "N",
+      "O", "O", "O", "O", "O", "O", "O", "O",
+      "P", "P",
+      "Q",
+      "R", "R", "R", "R", "R", "R",
+      "S", "S", "S", "S",
+      "T", "T", "T", "T", "T", "T",
+      "U", "U", "U", "U",
+      "V", "V",
+      "W", "W",
+      "X",
+      "Y", "Y",
+      "Z"
+    ];
+    let lettersInHand = []
+    for (let i = 0; i < 10; i++) {
+      let rand = letters.splice(Math.floor(Math.random() * letters.length), 1)[0];
+      hand.push(rand);
+    }
+    return lettersInHand;
   },
+
+  const = usesAvailableLetters?(input, lettersInHand) {
+    inputArray = input.split("");
+  }
 };
 
 // Do not remove this line or your tests will break!
